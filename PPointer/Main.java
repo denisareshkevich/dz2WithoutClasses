7e8704b3e415dedde869d324bf4501191265eb73
denisareshkevich <orehden@mail.ru>
2017-02-28 13:18:42 +0300

    Add files via upload

---
+++ PPointer/Main.java
@@ -0,0 +1,9 @@
+package PPointer;
+
+public class  Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle.diagonalLenth());
+        System.out.println(rectangle.square());
+    }
+}
